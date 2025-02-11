147d5bdd90eb88de92a73e7c03b27b7bf68adfeb
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2025-02-11 09:47:17 -0300

    feat: validação na página de cadastro

--- frontend/src/app/components/auth/cadastro/cadastro.component.ts
+++ frontend/src/app/components/auth/cadastro/cadastro.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormErrorComponent } from "../../shared/form-error/form-error.component";
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormErrorComponent],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  formData = this.fb.group({
+    email: ['', [Validators.required, Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    username: ['', Validators.required]
+  })
+
+  constructor(private fb: FormBuilder){}
+
+  fieldHasRequiredError(fieldName: string) {
+    return this.formData.get(fieldName)?.hasError('required') && this.formData.get(fieldName)?.touched;
+  }
+
+  fieldHasPatternError(fieldName: string) {
+    if (this.formData.get(fieldName)?.hasError('pattern') && !(this.formData.get(fieldName)?.hasError('required')) && this.formData.get(fieldName)?.touched) {
+      return true;
+    }
+    return false;
+  }
 
 }
