335becdec8a634d12d40c6ee801ca6fe3b7700dc
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2025-02-11 09:16:12 -0300

    feat: página de cadastro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home/home.component';
+import { CadastroComponent } from './components/auth/cadastro/cadastro.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'cadastro', component: CadastroComponent }
 ];
