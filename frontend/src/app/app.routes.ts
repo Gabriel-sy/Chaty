d074e785fde863f8581ceeca697fdca2958e0a8b
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2025-02-11 09:24:26 -0300

    feat: página de login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home/home.component';
 import { CadastroComponent } from './components/auth/cadastro/cadastro.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'cadastro', component: CadastroComponent }
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'login', component: LoginComponent }
 ];
