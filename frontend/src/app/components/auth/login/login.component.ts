334030297eba2abc760f8fbce7a6268bd32e7fff
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2025-02-11 09:53:24 -0300

    feat: validação na página de login

--- frontend/src/app/components/auth/login/login.component.ts
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormErrorComponent } from "../../shared/form-error/form-error.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormErrorComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  formData = this.fb.group({
+    email: ['', [Validators.required, Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
+    password: ['', Validators.required],
+  })
 
+  constructor(private fb: FormBuilder){}
+
+  fieldHasRequiredError(fieldName: string) {
+    return this.formData.get(fieldName)?.hasError('required') && this.formData.get(fieldName)?.touched;
+  }
 }
