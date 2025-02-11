08e3aa2e07fd600592e256aaf3efc9b9e1d67945
Gabriel Moura <77291941+Gabriel-sy@users.noreply.github.com>
2025-02-11 10:09:01 -0300

    feat: dialog de perfil

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home/home.component';
 import { CadastroComponent } from './components/auth/cadastro/cadastro.component';
 import { LoginComponent } from './components/auth/login/login.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cadastro', component: CadastroComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'user', component: ProfileComponent }
+
 ];
